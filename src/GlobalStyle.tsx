af9abb77589b82d09b46c74d9fffc28888384e7a
moolmin <zzmzzm0065@gmail.com>
2024-07-13 14:11:16 +0900

    feature: create new post page

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,10 +2,19 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 
+    @font-face {
+        font-family: 'DungGeunMo';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'DungGeunMo', sans-serif;
     }
 `;
 
--- src/pages/pc/CreateAlbum.tsx
+++ src/pages/pc/CreateAlbum.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import styles from './CreateAlbum.module.css';
 import Layout from '../../components/layout/Layout';
-import Window from '../../assets/images/window.png';
 
 const CreateAlbumPC: React.FC = () => {
   return (
     <Layout>
       <div className={styles.container}>
         <div className={styles.windowContainer}>
-          <img src={Window} alt="bookImg" />
+          <div className={styles.topBar}></div>
+          <div className={styles.innerContainer}>
+            <div className={styles.imageContainer}></div>
+            <div className={styles.inputRow}>
+              <input
+                type="text"
+                placeholder="작성자"
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="비밀번호"
+                className={styles.input}
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="제목"
+              className={styles.titleInput}
+            />
+            <select className={styles.dropdown}>
+              <option value="jeju-northeast">제주 북동쪽</option>
+              <option value="jeju-northwest">제주 북서쪽</option>
+              <option value="seogwipo-southeast">서귀포 남동쪽</option>
+              <option value="seogwipo-southwest">서귀포 남서쪽</option>
+            </select>
+            <textarea
+              placeholder="내용을 입력하세요"
+              className={styles.textArea}
+            ></textarea>
+          </div>
         </div>
       </div>
     </Layout>
