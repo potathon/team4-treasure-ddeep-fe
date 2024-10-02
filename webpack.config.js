1ca0071b1d2d2c387997af3f2c9c089e8e3db740
Sumin Kim <145218872+moolmin@users.noreply.github.com>
2024-10-02 19:01:31 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), 
   },
   devServer: {
+    disableHostCheck: true,
     allowedHosts: "all", // 모든 호스트를 허용하는 설정
     port: 3000, 
     open: true, 
